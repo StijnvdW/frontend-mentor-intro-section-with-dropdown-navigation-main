e8251ac5d8212ab817687afcb2c94874ddd32f3d
StijnvdW <stijnvanderwielen@gmail.com>
2022-08-12 17:30:04 +0200

    Add style guide settings to Tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: '375px',
+      xl: '1440px'
+    },
+    colors: {
+      'almostWhite': '#FAFAFA',
+      'lightGrey': '#ADADAD',
+      'mediumGrey': '#686868',
+      'black': '#151515',
+      'colorOne': '#726CEE',
+      'colorTwo': '#4BB1DA',
+      'colorThree': '#EDD556',
+      'colorFour': '#8E4CB6',
+      'pureWhite': '#FFFFFF'
+    },
+    fontFamily: {
+      'sans': ['Epilogue', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
