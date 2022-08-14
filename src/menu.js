417e25d10681026a08c1a0900be754322e937bbd
StijnvdW <stijnvanderwielen@gmail.com>
2022-08-14 14:01:18 +0200

    Mobile menu and dropdown improvement.

--- src/menu.js
+++ src/menu.js
@@ -7,3 +7,13 @@ function openMobileMenu(params) {
     let mobileMenu = document.getElementById('mobile-menu');
     mobileMenu.style.display = 'block';
 }
+
+function clickMobileDropdownMenu(id) {
+    let dropdown = document.getElementById(id);
+    let displayValue = dropdown.style.display;
+    if (displayValue == 'block') {
+        dropdown.style.display = 'none'
+    } else {
+        dropdown.style.display = 'block';
+    }
+}
